01b7a23f1811ac8dcb4370ea3cdd5c8e2cc375b6
Marcin <marcingolenia@gmail.com>
2020-12-22 19:06:03 +0100

    🎈 Fun with Type Driven Development - Foosball types prototype. 🎈

---
+++ src/Foosball/Foosball.fs
@@ -0,0 +1,37 @@
+﻿module Foosball1
+
+open System
+
+type GameId = bigint
+type Footballers = | Yellow | Black // Official championship's colors
+type Rules =
+    { PointsInSet: byte
+      SetsToWinToBeWinner: byte }
+
+type TeamName = TeamName of string
+type Team = TeamName * Footballers
+type Score = { By: Team; At: DateTime }
+type SetScores = { Number: byte; Scores: Score list }
+
+type OpenGame =
+    { Id: GameId
+      Teams: Team * Team
+      StartedAt: DateTime
+      Rules: Rules
+      Score: SetScores list }
+
+type FinishedGame =
+    { Id: GameId
+      Teams: Team * Team
+      StartedAt: DateTime
+      FinishedAt: DateTime
+      Rules: Rules
+      Score: SetScores list }
+
+type Game =
+    | Open of OpenGame
+    | Finished of FinishedGame
+
+type addPoint = OpenGame -> Team -> DateTime -> Game
+type openGame = Team * Team -> DateTime -> GameId -> OpenGame
+
--- src/Foosball/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Foosball
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
